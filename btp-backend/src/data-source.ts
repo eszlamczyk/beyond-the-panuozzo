d9cfd9e29f2fda40b894b53f47dec89dc1981203
Ernest <ernest.szlamczyk@swmansion.com>
2026-02-10 14:41:23 +0100

    ORM setup

---
+++ btp-backend/src/data-source.ts
@@ -0,0 +1,19 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT ?? '5432'),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: false,
+  migrations: [path.join(__dirname, '../priv/migrations/**/*{.js,.ts}')],
+  migrationsRun: false,
+  migrationsTransactionMode: 'all',
+});
