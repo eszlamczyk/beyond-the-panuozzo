e49babd462a87d650146d5523d659d851d25bfbf
Ernest <ernest.szlamczyk@swmansion.com>
2026-02-11 11:53:19 +0100

    format

--- btp-backend/src/app.module.ts
+++ btp-backend/src/app.module.ts
@@ -22,13 +22,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         username: configService.get<string>('DB_USERNAME'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_NAME'),
-        autoLoadEntities: true, 
+        autoLoadEntities: true,
         synchronize: false,
       }),
     }),
-    
-    AuthenticationModule, 
-    AuthorizationModule
+
+    AuthenticationModule,
+    AuthorizationModule,
   ],
   controllers: [AppController],
   providers: [AppService],
