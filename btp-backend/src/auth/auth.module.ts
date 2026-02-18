7efb2619bd284c256413a8de6a7caf956a1e9fde
sswrk <swierk.szymon99@gmail.com>
2026-02-18 13:03:11 +0100

    Remove the throttlers: wrapper key; pass the throttle config array directly to forRoot()

--- btp-backend/src/auth/auth.module.ts
+++ btp-backend/src/auth/auth.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ThrottlerModule } from '@nestjs/throttler';
+import { seconds, ThrottlerModule } from '@nestjs/throttler';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { AuthorizationModule } from './authorization/authorization.module';
 import { AuthController } from './auth.controller';
@@ -8,7 +8,7 @@ import { AuthController } from './auth.controller';
   imports: [
     AuthenticationModule,
     AuthorizationModule,
-    ThrottlerModule.forRoot({ throttlers: [{ ttl: 60_000, limit: 10 }] }),
+    ThrottlerModule.forRoot([{ ttl: seconds(60), limit: 10 }]),
   ],
   controllers: [AuthController],
   exports: [AuthenticationModule, AuthorizationModule],
